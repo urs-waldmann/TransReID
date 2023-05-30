4394341b07a0a4b271351f2c9aed64fa22301382
Urs Waldmann <urs.waldmann@uni-konstanz.de>
2023-05-30 10:02:26 +0200

    Add/change code for NeTePu dataset.

--- datasets/make_dataloader.py
+++ datasets/make_dataloader.py
@@ -13,6 +13,7 @@ import torch.distributed as dist
 from .occ_duke import OCC_DukeMTMCreID
 from .vehicleid import VehicleID
 from .veri import VeRi
+from .netepu import NeTePu
 __factory = {
     'market1501': Market1501,
     'dukemtmc': DukeMTMCreID,
@@ -20,6 +21,7 @@ __factory = {
     'occ_duke': OCC_DukeMTMCreID,
     'veri': VeRi,
     'VehicleID': VehicleID,
+    'netepu': NeTePu
 }
 
 def train_collate_fn(batch):
